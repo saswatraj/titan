eacf47743ecedec57ab3131e47c49193a551eb44
Saswat Raj <rajsaswa@amazon.com>
2020-01-09 21:59:29 -0800

    kill aready running process

--- bin/start.sh
+++ bin/start.sh
@@ -2,4 +2,5 @@
 ​
 [ -s "/.nvm/nvm.sh" ] && \. "/.nvm/nvm.sh"
 cd /var/www/html/website
+ps ax | grep "node ./bin/www" | grep -v grep | awk '{print $1}' | xargs kill -9
 nohup npm start > /tmp/nodelog.out 2>&1 &
