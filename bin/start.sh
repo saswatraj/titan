5e4bdbb263672d9104a9392109ff34cddbdbfa77
rajsaswa <rajsaswa@amazon.com>
2020-02-23 16:27:26 -0800

    Start as sudo process

--- bin/start.sh
+++ bin/start.sh
@@ -3,4 +3,4 @@
 [ -s "/.nvm/nvm.sh" ] && \. "/.nvm/nvm.sh"
 cd /var/www/html/website
 ps ax | grep "node ./bin/www" | grep -v grep | awk '{print $1}' | xargs kill -9
-nohup npm start > /tmp/nodelog.out 2>&1 &
+sudo nohup npm start > /tmp/nodelog.out 2>&1 &
